87ef969fbbd4779452492c2b3dd21f5c82a70160
Lakdar Arfento <arfento@gmail.com>
2023-10-16 17:53:10 +0700

    first commit and config

---
+++ app/src/main/java/com/pinto/storyappkt/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pinto.storyappkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+}
+
+rootProject.name = "StoryAppKt"
+include(":app")
+ 
